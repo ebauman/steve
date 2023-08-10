15b495550f12e659cc85c12fcd2b30952cab0049
Eamon Bauman <eamon@eamonbauman.com>
2023-08-09 20:46:37 -0500

    changed build script

--- scripts/build-bin.sh
+++ scripts/build-bin.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-CGO_ENABLED=0 go build -ldflags "-extldflags -static -s" -o ./bin/steve
+echo "Building for GOOS ${INPUT_GOOS} and GOARCH ${INPUT_GOARCH}"
+
+mkdir -p dist/
+
+for os in $(echo $INPUT_GOOS | tr ',' '\n')
+do
+  for arch in $(echo $INPUT_GOARCH | tr ',' '\n')
+  do
+    echo "Building for ${os}/${arch}"
+    CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "-extldflags -static -s" -o dist/steveui-$os-$arch
+  done
+done
